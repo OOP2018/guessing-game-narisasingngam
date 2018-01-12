a43a97612e7aad01a15b3551b58e829655f51279
narisasingngam <mmintttt@gmail.com>
2018-01-12 15:44:23 +0700

    done

--- src/GameConsole.java
+++ src/GameConsole.java
@@ -2,10 +2,14 @@ import java.util.Scanner;
 
 /** 
  *  Play guessing game on the console.
+ *  @author Narisa Singngam
  */
 public class GameConsole {
 
-	/** play the game. */
+	/** play the game. 
+	 * @param game receive NumberGame Object
+	 * @return the correct answer
+	 */
 	public int play(NumberGame game) {
 		Scanner console = new Scanner(System.in);
 		
@@ -13,12 +17,16 @@ public class GameConsole {
 		System.out.println( game.toString() );
 		
 		// This is just an example.
+		while(true){
 		System.out.println( game.getMessage() );
 		System.out.print("Your answer? ");
 		int guess = console.nextInt();
 		boolean correct = game.guess(guess);
-		System.out.println( game.getMessage() );
-		return guess;
+		if(correct){
+			return guess;
+		}
+		
+		}
 	}
 	
 }
